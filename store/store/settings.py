7c3de2a0b6d6d9786c7f5f47b65b93d7d2b1d1dc
Santiago Benavides <santiago.benavides@alumnos.udg.mx>
2021-10-18 19:36:58 -0500

    Static

--- store/store/settings.py
+++ store/store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gd3^xqa5gjwfuxg)e8xmc8+xr8s$!9t5ol!su%$6g1zw^%@keb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['santiagolib.herokuapp.com','127.0.0.1']
 
